504190f3668520bed308efc158f9bd6881ecdf1c
shreysh-mane <shreysh.mane12@gmail.com>
2021-08-22 14:20:08 +0530

    fix-links added

---
+++ blog/blog1/Arrays.js
@@ -0,0 +1,12 @@
+
+let fruits = ["apple", "banana", "mango", "kiwi"];
+
+for (let fruit in fruits) {
+  console.log(fruit);
+}
+//0
+//1
+//2
+//3
+
+
---
+++ blog/blog2/Arrays.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "banana", "mango", "kiwi"];
+
+for (let fruit in fruits) {
+  console.log(fruit);
+}
+//0
+//1
+//2
+//3
